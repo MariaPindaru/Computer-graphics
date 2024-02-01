5add3b4b65b967f3dc286f71cbf8163c8b8985e0
Pîndaru Maria <65163209+MariaPindaru@users.noreply.github.com>
2024-02-01 23:45:28 +0200

    [Tema6] Add inplementation

--- Tema6/Tema6Dlg.cpp
+++ Tema6/Tema6Dlg.cpp
@@ -12,25 +12,35 @@
 #define new DEBUG_NEW
 #endif
 
+#define DOT_SIZE 7
 
 // CTema6Dlg dialog
 
 
 
-CTema6Dlg::CTema6Dlg(CWnd* pParent /*=nullptr*/)
-	: CDialogEx(IDD_TEMA6_DIALOG, pParent)
+CTema6Dlg::CTema6Dlg( CWnd* pParent /*=nullptr*/ )
+	: CDialogEx( IDD_TEMA6_DIALOG, pParent )
+	, m_bCollectingPoints( true )
+	, m_selectedPoint( nullptr )
 {
-	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+	m_hIcon = AfxGetApp()->LoadIcon( IDR_MAINFRAME );
 }
 
-void CTema6Dlg::DoDataExchange(CDataExchange* pDX)
+void CTema6Dlg::DoDataExchange( CDataExchange* pDX )
 {
-	CDialogEx::DoDataExchange(pDX);
+	CDialogEx::DoDataExchange( pDX );
 }
 
-BEGIN_MESSAGE_MAP(CTema6Dlg, CDialogEx)
+BEGIN_MESSAGE_MAP( CTema6Dlg, CDialogEx )
 	ON_WM_PAINT()
 	ON_WM_QUERYDRAGICON()
+
+	ON_WM_LBUTTONDOWN()
+	ON_WM_LBUTTONUP()
+
+	ON_WM_RBUTTONDOWN()
+
+	ON_WM_MOUSEMOVE()
 END_MESSAGE_MAP()
 
 
@@ -42,10 +52,10 @@ BOOL CTema6Dlg::OnInitDialog()
 
 	// Set the icon for this dialog.  The framework does this automatically
 	//  when the application's main window is not a dialog
-	SetIcon(m_hIcon, TRUE);			// Set big icon
-	SetIcon(m_hIcon, FALSE);		// Set small icon
+	SetIcon( m_hIcon, TRUE );			// Set big icon
+	SetIcon( m_hIcon, FALSE );		// Set small icon
 
-	// TODO: Add extra initialization here
+	SetWindowPos( NULL, 0, 0, 700, 600, SWP_NOMOVE | SWP_NOZORDER );
 
 	return TRUE;  // return TRUE  unless you set the focus to a control
 }
@@ -56,26 +66,21 @@ BOOL CTema6Dlg::OnInitDialog()
 
 void CTema6Dlg::OnPaint()
 {
-	if (IsIconic())
-	{
-		CPaintDC dc(this); // device context for painting
+	CPaintDC dc( this );
+	//dc.SetTextColor( RGB( 255, 0, 0 ) );
 
-		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+	for ( const auto& point : m_points ) {
+		CBrush redBrush( RGB( 255, 0, 0 ) );
+		dc.SelectObject( &redBrush );
+		dc.Ellipse( point.x - DOT_SIZE, point.y - DOT_SIZE, point.x + DOT_SIZE, point.y + DOT_SIZE );
 
-		// Center icon in client rectangle
-		int cxIcon = GetSystemMetrics(SM_CXICON);
-		int cyIcon = GetSystemMetrics(SM_CYICON);
-		CRect rect;
-		GetClientRect(&rect);
-		int x = (rect.Width() - cxIcon + 1) / 2;
-		int y = (rect.Height() - cyIcon + 1) / 2;
-
-		// Draw the icon
-		dc.DrawIcon(x, y, m_hIcon);
+		/*	CString str;
+			str.Format( _T( "(%d, %d)" ), point.x, point.y );
+			dc.TextOut( point.x + 10, point.y, str );*/
 	}
-	else
-	{
-		CDialogEx::OnPaint();
+
+	if ( !m_bCollectingPoints ) {
+		DrawCurve( &dc );
 	}
 }
 
@@ -83,6 +88,146 @@ void CTema6Dlg::OnPaint()
 //  the minimized window.
 HCURSOR CTema6Dlg::OnQueryDragIcon()
 {
-	return static_cast<HCURSOR>(m_hIcon);
+	return static_cast< HCURSOR >( m_hIcon );
 }
 
+void CTema6Dlg::OnRButtonDown( UINT nFlags, CPoint point )
+{
+	if ( m_points.size() > 1 && m_bCollectingPoints )
+	{
+		m_bCollectingPoints = false;
+		Invalidate();
+	}
+
+	CDialogEx::OnRButtonDown( nFlags, point );
+}
+
+void CTema6Dlg::OnLButtonDown( UINT nFlags, CPoint point )
+{
+	if ( m_bCollectingPoints )
+	{
+		if ( m_points.empty() || point.x > m_points.back().x ) {
+			m_points.push_back( point );
+		}
+	}
+	else
+	{
+		for ( size_t index = 0; index < m_points.size(); ++index )
+		{
+			if ( abs( point.x - m_points[index].x ) < 5 && abs( point.y - m_points[index].y ) < 5 )
+			{
+				m_selectedPoint = &m_points[index];
+				break;
+			}
+		}
+	}
+
+	Invalidate();
+
+	CDialogEx::OnLButtonDown( nFlags, point );
+}
+
+void CTema6Dlg::OnLButtonUp( UINT nFlags, CPoint point )
+{
+	m_selectedPoint = nullptr;
+}
+
+void CTema6Dlg::OnMouseMove( UINT nFlags, CPoint point )
+{
+	if ( !m_selectedPoint )
+		return;
+
+	if ( m_selectedPoint != &m_points.front() )
+	{
+		CPoint* prev = ( m_selectedPoint - 1 );
+		if ( prev->x > point.x )
+			return;
+	}
+
+	if ( m_selectedPoint != &m_points.back() )
+	{
+		CPoint* next = ( m_selectedPoint + 1 );
+		if ( next->x < point.x )
+			return;
+	}
+
+	m_selectedPoint->x = point.x;
+	m_selectedPoint->y = point.y;
+
+	Invalidate();
+}
+
+std::vector<double> ComputeNewtonCoefficients( const std::vector<double>& x, const std::vector<double>& y )
+{
+	int pointsCount = x.size();
+	std::vector<std::vector<double>> a( pointsCount, std::vector<double>( pointsCount ) );
+
+
+	//  i = j  => aij = Y[i]i
+	for ( int i = 0; i < pointsCount; ++i )
+	{
+		a[i][i] = y[i];
+	}
+
+	//	i != j => aij = a[i + 1][j] - a[i][j - 1] / x[j] - x[i], 0 <= i <= j <= n
+	for ( int i = pointsCount - 1; i >= 0; --i )
+	{
+		for ( size_t j = i + 1; j < pointsCount; ++j )
+		{
+			a[i][j] = ( a[i + 1][j] - a[i][j - 1] ) / ( x[j] - x[i] );
+		}
+	}
+
+	std::vector<double> coefficients;
+	for ( int j = 0; j < pointsCount; ++j )
+	{
+		coefficients.push_back( a[0][j] );
+	}
+
+	return coefficients;
+}
+
+double GetNewtonPolynomial( const std::vector<double>& x, const std::vector<double>& coefficients, double xInterpolate ) 
+{
+	double result = 0;
+	double product = 1;
+
+	for ( int i = 0; i < x.size(); ++i )
+	{
+		result += coefficients[i] * product;
+		product *= ( xInterpolate - x[i] );
+	}
+
+	return result;
+}
+
+
+void CTema6Dlg::DrawCurve( CDC* pDC )
+{
+	std::vector<double> xCoordinates, yCoordinates;
+	xCoordinates.reserve( m_points.size() );
+	yCoordinates.reserve( m_points.size() );
+
+	for ( CPoint point : m_points )
+	{
+		xCoordinates.push_back( point.x );
+		yCoordinates.push_back( point.y );
+	}
+
+	CPen pen( 0, 3, RGB( 0, 0, 0 ) );
+	pDC->SelectObject( &pen );
+
+	// start from the first point 
+	pDC->MoveTo( m_points.front() );
+
+	const std::vector<double> coefficients = ComputeNewtonCoefficients( xCoordinates, yCoordinates );
+
+	for ( double xInterpolate = xCoordinates.front(); xInterpolate <= xCoordinates.back(); xInterpolate += 0.1 )
+	{
+		double yInterpolate = GetNewtonPolynomial( xCoordinates, coefficients, xInterpolate );
+
+		pDC->LineTo( xInterpolate, yInterpolate );
+		// prepare for next one
+		pDC->MoveTo( xInterpolate, yInterpolate );
+	}
+}
--- Tema6/Tema6Dlg.h
+++ Tema6/Tema6Dlg.h
@@ -4,6 +4,7 @@
 
 #pragma once
 
+#include <vector>
 
 // CTema6Dlg dialog
 class CTema6Dlg : public CDialogEx
@@ -29,5 +30,17 @@ protected:
 	virtual BOOL OnInitDialog();
 	afx_msg void OnPaint();
 	afx_msg HCURSOR OnQueryDragIcon();
+	afx_msg void OnRButtonDown( UINT nFlags, CPoint point );
+	afx_msg void OnLButtonDown( UINT nFlags, CPoint point );
+	afx_msg void OnLButtonUp( UINT nFlags, CPoint point );
+	afx_msg void OnMouseMove( UINT nFlags, CPoint point );
 	DECLARE_MESSAGE_MAP()
+
+private:
+	void DrawCurve( CDC* pDC );
+
+private:
+	std::vector<CPoint> m_points;
+	CPoint* m_selectedPoint;
+	bool m_bCollectingPoints;
 };
